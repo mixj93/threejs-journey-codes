f8cf7f988a51c258755ce80b019ca765b56d0052
Li Xueli <mixj93@163.com>
2021-05-16 12:04:23 +0800

    finish 05

--- 05-transforms-objects/src/script.js
+++ 05-transforms-objects/src/script.js
@@ -10,21 +10,26 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-// Position
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }))
+group.add(cube1)
 
-mesh.position.set(0.7, -0.6, 1)
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x00ff00 }))
+cube2.position.x = -2
+group.add(cube2)
 
-// Scaler
-scene.add(mesh)
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0x0000ff }))
+cube3.position.x = 2
+group.add(cube3)
 
 // Axes helper
+// https://threejs.org/docs/#api/en/helpers/AxesHelper
+// The X axis is red. The Y axis is green. The Z axis is blue.
 const axesHelper = new THREE.AxesHelper(3)
 scene.add(axesHelper)
 
@@ -42,6 +47,8 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
